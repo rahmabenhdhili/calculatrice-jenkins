29e0ca44f4e732129b6464c6354f103517f7dde0
Rahma Hdhili <rahma1.benhdhili@gmail.com>
2025-11-21 22:04:10 +0100

    modifier js

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const display = document.getElementById('display');
 
+// Ajoute une valeur à l'écran
 function appendValue(val) {
     display.value += val;
 }
 
+// Efface entièrement l'écran
 function clearDisplay() {
     display.value = '';
 }
 
+// Supprime le dernier caractère (optionnel mais utile)
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Calcule le résultat
 function calculate() {
     try {
-        display.value = eval(display.value);
+        // Vérifie que l’expression contient seulement chiffres et opérateurs
+        if (/^[0-9+\-*/(). ]+$/.test(display.value)) {
+            display.value = eval(display.value);
+        } else {
+            display.value = 'Erreur';
+        }
     } catch {
         display.value = 'Erreur';
     }
